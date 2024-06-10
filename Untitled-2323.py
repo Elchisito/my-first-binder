afa511a06e3b1d7874e567889f49900eb94f6392
Chisi <74630522+Elchisito@users.noreply.github.com>
2024-06-10 01:14:41 -0300

    Add files via upload

---
+++ Untitled-2323.py
@@ -0,0 +1,9 @@
+mina_juanco = int(input("Ingrese el kilaje de la mujer a comerse de juancho "))
+mina_juanco = 1
+while mina_juanco < 100 :
+    mina_juanco = int(input("Ingrese el kilaje de la mujer a comerse de juancho "))
+    
+    if mina_juanco >= 100 :
+        print("Juancho mete lenguetazo")
+    else:
+        print("Poco peso para el rango del querido juancho")
